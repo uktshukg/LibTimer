8a2d6cc148d4a4ce31db1e3f4576a16f756f25bb
Utkarsh Shukla <ushukla.iitr@gmail.com>
2020-10-24 08:59:15 +0530

    - add multidex

--- app/src/main/java/com/dexter/baseproject/App.kt
+++ app/src/main/java/com/dexter/baseproject/App.kt
@@ -2,6 +2,9 @@ package com.dexter.baseproject
 
 import android.app.Activity
 import android.app.Application
+import android.content.Context
+import androidx.multidex.MultiDex
+import androidx.multidex.MultiDexApplication
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.HasAndroidInjector
@@ -17,6 +20,11 @@ class App : Application(), HasAndroidInjector {
         DaggerAppComponent.builder().applicationBind(this).build().inject(this)
 
     }
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
 
     override fun androidInjector(): AndroidInjector<Any> = dispatchingAndroidInjector
 
